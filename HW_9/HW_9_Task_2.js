0238c3116870bf8b2361cc06d00d8d4852026358
Zarina <100086150+NZarina@users.noreply.github.com>
2024-10-20 00:02:28 +0300

    Update HW_9_Task_2.js
    Добавила дополнительные проверки для задач 1, 2, 4, 5

--- HW_9/HW_9_Task_2.js
+++ HW_9/HW_9_Task_2.js
@@ -8,18 +8,28 @@ const characters = [
 // 1. Напишите функцию addCharacter(character) позволяющую добавить новый объект в массив characters. 
 //     Объект должен иметь поля name (string) и age (number)
 function addCharacter(character) {
-    const {name, age} = character;
-    characters.push(character);
+    if(character.hasOwnProperty('name') && character.hasOwnProperty('age') && typeof character.name === 'string' && typeof character.age === 'number') {
+      characters.push(character);
+    } else {
+      console.log("ОШИБКА! Объект должен содержать параметр name с типом данных string и параметр age с типом данных number.");
+    }
 }
+
 addCharacter({ name: 'Sofia', age: 25 });
 
 
 // 2. Напишите функцию getCharacter(name), позволяющую получить объект персонажа по его имени// getCharacter('Fred') => { 'name': 'Fred', 'age': 40 }
 function getCharacter(name) {
-    return characters.find((character) => character.name == name);
+    const found = characters.find((character) => character.name == name); 
+    if (found) {
+      return found;
+    } else {
+        console.log(`Персонаж с именем ${name} не найден.`);
+    };
 };
 
-// console.log(getCharacter("Fred")); // { name: 'Fred', age: 40 } 
+getCharacter("Fred"); // { name: 'Fred', age: 40 } 
+
 
 // 3. Напишите функцию getCharactersByAge(minAge), возвращающую массив персонажей НЕ МЛАДШЕ minAge // getCharactersByAge(40) => [{ 'name': 'Fred', 'age': 40 },{ 'name': 'Jack', 'age': 50 }]
 function getCharactersByAge(minAge) {
@@ -28,26 +38,36 @@ function getCharactersByAge(minAge) {
 
 // console.log(getCharactersByAge(40)); // [ { name: 'Fred', age: 40 }, { name: 'Jack', age: 50 } ]
 
+
 // 4. Напишите функцию updateCharacter(name, newCharacter). (Методом getCharacter(name) получаем ссылку на нужного персонажа, а потом меняем ему данные)
 function updateCharacter(name, newCharacter) {
-    const characterToChange = getCharacter(name); // находим заменяемого персонажа по параметру name
-    for (const character of characters) { 
-        if (character.name === characterToChange.name) { // если имя персонажа в массиве равно имени заменяемого персонажа
-            characterToChange.name = newCharacter.name; // меняем имя заменяемого персонажа на имя нового персонажа
-            characterToChange.age = newCharacter.age; // меняем возраст заменяемого персонажа на возраст нового персонажа
-        } 
-    } return characters; // возвращаем массив с персонажами
-}
+    if (typeof name !== 'string') {
+      console.log('Введите имя заменяемого персонажа в формате string')
+    }  
+    if (typeof newCharacter !== 'object') {
+      console.log('Введите данные в формате object');
+    };
+    if(!newCharacter.hasOwnProperty('name') || !newCharacter.hasOwnProperty('age') || typeof newCharacter.name !== 'string' || typeof newCharacter.age !== 'number') {
+      console.log('Объект должен содержать параметр name с типом данных string и параметр age с типом данных number.');
+    }
+    const characterToChange = getCharacter(name);
+    if (characterToChange) {
+      characterToChange.name = newCharacter.name;
+      characterToChange.age = newCharacter.age;  
+    }
+  }
 
 updateCharacter('Fred',  { 'name': 'Tom', 'age': 35 });
-console.log(characters);
 
 
 // 5. Напишите функцию для удаления персонажа removeCharacter(name) (Реализовать через splice, индекс персонажа искать методом findInxex)
 function removeCharacter(name) {
     const index = characters.findIndex((character) => character.name === name);
-    characters.splice(index, 1);
-    return characters; 
+    if(index >= 0) {
+        characters.splice(index, 1);
+    } else {
+        console.log('Персонаж не найден');
+    }
 }
+
 removeCharacter('Jack');
-console.log(characters);
